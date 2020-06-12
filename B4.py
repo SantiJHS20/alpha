00f9b9d8d2fa7fd73c337438b698f752c5880875
Santiago <santiagojhs2004@gmail.com>
2020-06-12 15:36:47 -0500

    Break

--- B4.py
+++ B4.py
@@ -43,8 +43,4 @@ while opcion_de_menu != 0:
 f2.guardar_datos(nombre, edad, estatura, sexo, pais, num_amigos)
 
 print("Gracias por usar Break, vuelve pronto!!!")
-print("commit 2")
-print("commit 3")
-x = 7
-a = 7
-print(a**x)
+
